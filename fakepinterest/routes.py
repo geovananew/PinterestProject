74bb2ee6b4eedbe882799d9fa93231a1084b4503
geovananew <geovananewtesc@gmail.com>
2023-05-25 08:01:37 -0300

    nova alteração

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -3,7 +3,9 @@ from flask import render_template, url_for, redirect
 from fakepinterest import app, database, bcrypt
 from fakepinterest.models import Usuario, Foto
 from flask_login import login_required, login_user, logout_user, current_user
-from fakepinterest.forms import FormLogin, FormCriarConta
+from fakepinterest.forms import FormLogin, FormCriarConta, FormFoto
+from werkzeug.utils import secure_filename
+import os
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
@@ -29,18 +31,36 @@ def criarconta():
     return render_template("criarconta.html", form=form_criarconta)
 
 # criar outra aba
-@app.route("/perfil/<id_usuario>")
+@app.route("/perfil/<id_usuario>", methods=["GET", "POST"])
 @login_required
 def perfil(id_usuario):
     if int(id_usuario) == int(current_user.id):
         #o usuario ta vendo o perfil dele
-        return render_template("perfil.html", usuario=current_user)
+        form_foto = FormFoto()
+        if form_foto.validate_on_submit():
+            arquivo = form_foto.foto.data
+            nome_seguro = secure_filename(arquivo.filename)
+            #salvar o arquivo na pasta fotos_post
+            caminho = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                   app.config["UPLOAD_FOLDER"],nome_seguro)
+            arquivo.save(caminho)
+            #registrar esse arquivo no banco de dados
+            foto = Foto(imagem=nome_seguro, id_usuario=current_user.id)
+            database.session.add(foto)
+            database.session.commit()
+        return render_template("perfil.html", usuario=current_user, form=form_foto)
     else:
         usuario = Usuario.query.get(int(id_usuario))
-        return render_template("perfil.html", usuario=usuario)
+        return render_template("perfil.html", usuario=usuario, form=None)
 
 @app.route("/logout")
 @login_required
 def logout ():
     logout_user()
     return redirect(url_for("homepage"))
+
+@app.route("/feed")
+@login_required
+def feed():
+    fotos= Foto.query.order_by(Foto.data_criacao.desc()).all()
+    return render_template("feed.html", fotos=fotos)
